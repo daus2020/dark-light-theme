c3214594d5fcd4ba824422c2de6d103a0d18bc38
danielus <danielandresus@gmail.com>
2024-07-17 22:01:25 -0400

    preparing to github pages

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import svgr from "vite-plugin-svgr";
 export default defineConfig({
   // plugins: [react()],
   plugins: [react(), svgr()],
+  base: "https://daus2020.github.io/dark-light-theme",
 });
